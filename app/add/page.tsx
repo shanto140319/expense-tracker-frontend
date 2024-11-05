b6278edcce2254a9ebc5bb1ecd7ea53c865600a3
Jahidul Haque Santo <shanto@konasl.com>
2024-11-05 17:33:53 +0600

    deployment issue

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -7,7 +7,7 @@ import {Textarea} from "@/components/ui/textarea"
 import api from "@/lib/api"
 import Link from "next/link"
 import {useRouter, useSearchParams} from "next/navigation"
-import {useState} from "react"
+import {Suspense, useState} from "react"
 import toast from "react-hot-toast"
 import AddCategoryModal from "../components/AddCategoryModal"
 import Categories from "../components/Categories"
@@ -71,62 +71,68 @@ const Page = () => {
     }
 
     return (
-        <section className='grid w-full mt-10 gap-5'>
-            <Link href={"/"}>
-                <Button>Back to home</Button>
-            </Link>
-            <div className='grid gap-1.5'>
-                <Label htmlFor='amount'>Amount</Label>
-                <Input
-                    type='number'
-                    id='amount'
-                    placeholder='Enter amount'
-                    value={transaction.amount}
-                    onChange={handleChange}
-                    min='0'
-                />
-            </div>
-            <div className='grid gap-1.5'>
-                <Label htmlFor='description'>Description</Label>
-                <Textarea
-                    placeholder='Enter description'
-                    id='description'
-                    value={transaction.description}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className='grid gap-1.5'>
-                <Label htmlFor='category' className='flex flex-col gap-5'>
-                    Categories:{" "}
-                    <div className='flex items-center gap-5 flex-wrap'>
-                        <Button onClick={() => setIsOpen(true)}>
-                            Create Category
-                        </Button>
-                        <Button onClick={() => setIsDeleteOpen(!isDeleteOpen)}>
-                            {!isDeleteOpen ? "Delete Category" : "Don't Delete"}
-                        </Button>
-                    </div>
-                </Label>
-                <Categories
-                    isDeleteOpen={isDeleteOpen}
-                    addCategorySuccess={addCategorySuccess}
-                    selectedCategory={selectedCategory}
-                    setSelectedcategory={setSelectedCategory}
+        <Suspense fallback={<div>Loading...</div>}>
+            <section className='grid w-full mt-10 gap-5'>
+                <Link href={"/"}>
+                    <Button>Back to home</Button>
+                </Link>
+                <div className='grid gap-1.5'>
+                    <Label htmlFor='amount'>Amount</Label>
+                    <Input
+                        type='number'
+                        id='amount'
+                        placeholder='Enter amount'
+                        value={transaction.amount}
+                        onChange={handleChange}
+                        min='0'
+                    />
+                </div>
+                <div className='grid gap-1.5'>
+                    <Label htmlFor='description'>Description</Label>
+                    <Textarea
+                        placeholder='Enter description'
+                        id='description'
+                        value={transaction.description}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='grid gap-1.5'>
+                    <Label htmlFor='category' className='flex flex-col gap-5'>
+                        Categories:{" "}
+                        <div className='flex items-center gap-5 flex-wrap'>
+                            <Button onClick={() => setIsOpen(true)}>
+                                Create Category
+                            </Button>
+                            <Button
+                                onClick={() => setIsDeleteOpen(!isDeleteOpen)}
+                            >
+                                {!isDeleteOpen
+                                    ? "Delete Category"
+                                    : "Don't Delete"}
+                            </Button>
+                        </div>
+                    </Label>
+                    <Categories
+                        isDeleteOpen={isDeleteOpen}
+                        addCategorySuccess={addCategorySuccess}
+                        selectedCategory={selectedCategory}
+                        setSelectedcategory={setSelectedCategory}
+                        transactionType={paramValue}
+                    />
+                </div>
+
+                <AddCategoryModal
+                    isOpen={isOpen}
+                    setIsOpen={setIsOpen}
+                    setAddCategorySuccess={setAddCategorySuccess}
                     transactionType={paramValue}
                 />
-            </div>
 
-            <AddCategoryModal
-                isOpen={isOpen}
-                setIsOpen={setIsOpen}
-                setAddCategorySuccess={setAddCategorySuccess}
-                transactionType={paramValue}
-            />
-
-            <Button className='mt-5' onClick={handleAddTransaction}>
-                Add {paramValue}
-            </Button>
-        </section>
+                <Button className='mt-5' onClick={handleAddTransaction}>
+                    Add {paramValue}
+                </Button>
+            </section>
+        </Suspense>
     )
 }
 
